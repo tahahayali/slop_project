473211959407c3237b27564e3d80033dd41fb2f7
tahahayali <tahahayali05@gmail.com>
2026-02-02 14:06:09 -0500

    Feb 2nd. I restarted the project because theres a lot more react I need to learn

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <Text />
+      <Text />
+    </div>
+  )
+}
+
+function Text() {
+  return <h1>Slop</h1>
+}
+
+
+export default App
